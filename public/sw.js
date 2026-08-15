94dea2296f9c947a1cf802289293b6b59ac01f66
iboulahifall <iboulahifall97@gmail.com>
2026-08-15 23:24:47 +0200

    ix chemins icones

--- public/sw.js
+++ public/sw.js
@@ -10,8 +10,8 @@ const SHELL = [
   "/",
   "/index.html",
   "/manifest.webmanifest",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png",
+  "/icon-192.png",
+  "/icon-512.png",
 ];
 
 self.addEventListener("install", (event) => {
